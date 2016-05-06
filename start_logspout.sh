f1bf49c235655f7e20b69fa912b71d81cc7ce786
Vincent Boutour <vincent.boutour@gmail.com>
2016-05-06 12:56:34 +0000

    Fixing typo mistakes

--- start_logspout.sh
+++ start_logspout.sh
@@ -3,4 +3,4 @@
 
 export LOGSTASH_IP=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' --type=container elk_logstash_1`
 docker-compose -p logspout -f docker-compose-swarm-logspout.yml up -d
-docker-compose -p logspout -f docker-compose-swarm-logspout scale logspout=`docker info 2>/dev/null | grep ^Nodes: | awk '{print $2}'`
+docker-compose -p logspout -f docker-compose-swarm-logspout.yml scale logspout=`docker info 2>/dev/null | grep ^Nodes: | awk '{print $2}'`
