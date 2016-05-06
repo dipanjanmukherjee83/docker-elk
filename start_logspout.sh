3cc34b1303595fb74fee9fbb7a2997af45a102d1
Vincent Boutour <vincent.boutour@gmail.com>
2016-05-06 10:50:05 +0200

    Adding command to start logspout

---
+++ start_logspout.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+export LOGSTASH_IP=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' --type=container elk_logstash_1`
+docker-compose -p logspout -f docker-compose-swarm-logspout.yml up -d
+docker-compose -p logspout -f docker-compose-swarm-logspout scale logspout=`docker info 2>/dev/null | grep ^Nodes: | awk '{print $2}'`
