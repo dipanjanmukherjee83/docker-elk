47a2e84ceb85998c8bac621d5e4e1cebc4b39791
Vincent Boutour <ViBiOh@users.noreply.github.com>
2016-05-13 11:22:15 +0200

    Update start_logspout.sh

--- start_logspout.sh
+++ start_logspout.sh
@@ -28,5 +28,5 @@ function docker-clean() {
 
 docker-clean logspout* force
 export LOGSTASH_IP=`docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' --type=container elk_logstash_1`
-docker-compose -p logspout -f docker-compose-swarm-logspout.yml up -d
-docker-compose -p logspout -f docker-compose-swarm-logspout.yml scale logspout=`docker info 2>/dev/null | grep ^Nodes: | awk '{print $2}'`
+docker-compose -p logspout -f docker-compose-swarm-logspout.yml -f docker-compose-swarm-logspout.swarm.yml up -d
+docker-compose -p logspout -f docker-compose-swarm-logspout.yml -f docker-compose-swarm-logspout.swarm.yml scale logspout=`docker info 2>/dev/null | grep ^Nodes: | awk '{print $2}'`
